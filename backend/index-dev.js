cedd1cc883eff51f6ad2ed90a4b11b102c42c0fc
Ramasubramanian M <vinothsubramanian@gmail.com>
2019-05-07 20:02:41 +0530

    Changed node to nodemon for dev version

--- backend/index-dev.js
+++ backend/index-dev.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 const apis = require("./index");
+const apiPort = 3000;
 
 app.use(
   '/api/**',
   apis
 );
-app.listen(3000);
+app.listen(apiPort);
+console.log(`Api is listening in port: ${apiPort}`);
